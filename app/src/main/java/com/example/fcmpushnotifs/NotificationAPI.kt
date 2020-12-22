4f45aa1ac782ee064c26c712b25293e84c2bc195
Pierre Shum <pshum1@hawk.iit.edu>
2020-12-22 10:20:39 -0600

    removed API key

--- app/src/main/java/com/example/fcmpushnotifs/NotificationAPI.kt
+++ app/src/main/java/com/example/fcmpushnotifs/NotificationAPI.kt
@@ -10,6 +10,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.Headers
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface NotificationAPI {
 
